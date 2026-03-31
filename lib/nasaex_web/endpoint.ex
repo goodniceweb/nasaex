ab8608223fea3da831df215ba40f87f9394d3a41
Oleksii Cherkashyn <goodniceweb@gmail.com>
2026-03-31 16:20:29 +0300

    Install tidewave

--- lib/nasaex_web/endpoint.ex
+++ lib/nasaex_web/endpoint.ex
@@ -27,6 +27,10 @@ defmodule NasaexWeb.Endpoint do
     only: NasaexWeb.static_paths(),
     raise_on_missing_only: code_reloading?
 
+  if Mix.env() == :dev do
+    plug Tidewave, team: [id: System.get_env("TIDEWAVE_TEAM"), token: System.get_env("TIDEWAVE_TEAM_TOKEN")]
+  end
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
